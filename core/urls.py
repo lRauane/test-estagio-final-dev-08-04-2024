728a8cd33738375a4c8c784806ea99b30f76b8d3
Rauane Lima <rauanelima7@gmail.com>
2024-04-08 10:47:28 -0300

    Added HTML interface for calculator and styling

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from consumer.views import ConsumerViewSet, CalculatorAPIView
+from consumer.views import ConsumerViewSet, CalculatorAPIView, CalculatorInterfaceView
 
 router = DefaultRouter()
 router.register(r'consumers', ConsumerViewSet)
 
 urlpatterns = [
 	path('api/calculator/', CalculatorAPIView.as_view(), name="calculator"),
+	path('calculator/', CalculatorInterfaceView.as_view(), name='calculator_interface'),
 ]
