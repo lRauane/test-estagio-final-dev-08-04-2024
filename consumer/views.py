1459f643f9e188804cb5218edad5bda7ca1fbba7
Rauane Lima <rauanelima7@gmail.com>
2024-04-08 10:29:48 -0300

    FEAT: Added API route for calculator functionality

--- consumer/views.py
+++ consumer/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework import viewsets, status, response, request
 from .models import Consumer
 from .serializers import ConsumerSerializer
+from rest_framework.response import Response
+from django.views.generic import TemplateView
+from calculator import calculator
 
 class ConsumerViewSet(viewsets.ModelViewSet):
     queryset = Consumer.objects.all()
     serializer_class = ConsumerSerializer
+	
+class CalculatorAPIView(APIView):
+    def post(self, request):
+        data = request.data
+        consumption_month1 = int(data.get('consumption_month1'))
+        consumption_month2 = int(data.get('consumption_month2'))
+        consumption_month3 = int(data.get('consumption_month3'))
+        distributor_tax = float(data.get('distributor_tax'))
+        tax_type = data.get('tax_type')
+        
+        result = calculator([consumption_month1, consumption_month2, consumption_month3], distributor_tax, tax_type)  # Use o módulo importado corretamente
+        return Response({
+            'annual_savings': result[0],
+            'monthly_savings': result[1],
+            'applied_discount': result[2],
+            'coverage': result[3]
+        }, status=status.HTTP_200_OK)
+	
+    def get(self, request):
+        return Response({'result': 'hello'}, status=status.HTTP_200_OK)
+
+class CalculatorInterfaceView(TemplateView):
+    template_name = 'calculator_interface.html'
--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,10 @@
-from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from consumer.views import ConsumerViewSet
+from consumer.views import ConsumerViewSet, CalculatorAPIView
 
 router = DefaultRouter()
 router.register(r'consumers', ConsumerViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-	path('', include(router.urls))
+	path('api/calculator/', CalculatorAPIView.as_view(), name="calculator"),
 ]
